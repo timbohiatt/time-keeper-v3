928479cbfc5b9882fe2e272abf05db2bd3cc103e
Tim Hiatt <timhiatt@google.com>
2022-12-05 21:57:04 +0000

    Service Annotations

--- ops/setup.sh
+++ ops/setup.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+kubectl delete namespace istio-system 
+sleep 10s
 kubectl apply -f istio/namespaces/namespace.yaml
 kubectl apply -f istio/install-manifests.yaml || true
 kubectl apply -f istio/install-manifests.yaml || true
 kubectl apply -f istio/plugins --recursive || true
 
+sleep 10s
 kubectl get namespaces 
+
+sleep 10s
 kubectl get pods
