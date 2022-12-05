ea96c56d8e3828e0e1e9284c724f8eb639e6e372
Tim Hiatt <timhiatt@google.com>
2022-12-05 22:24:19 +0000

    Service Annotations

--- ops/apps/setup.sh
+++ ops/apps/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+kubectl delete namespace app-time-now
+sleep 10s
+
 kubectl apply -f apps/namespaces --recursive || true
 sleep 10s
 kubectl apply -f apps/gateways --recursive || true
