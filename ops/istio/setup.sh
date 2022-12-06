c784ce2ed52553a6bc2237ec10c7e6170a0b9e08
Tim Hiatt <timhiatt@google.com>
2022-12-06 10:12:57 +0000

    Service Annotations

--- ops/istio/setup.sh
+++ ops/istio/setup.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 kubectl delete namespace istio-system 
+kubectl delete namespace ops-argocd 
 sleep 30s
 kubectl apply -f istio/namespaces/namespace.yaml
 kubectl apply -f istio/install-manifests.yaml || true
 kubectl apply -f istio/install-manifests.yaml || true
 kubectl apply -f istio/plugins --recursive || true
-
 sleep 10s
-kubectl get namespaces 
-
-sleep 10s
-kubectl get pods
+kubectl apply -f argocd/ --recursive || true
