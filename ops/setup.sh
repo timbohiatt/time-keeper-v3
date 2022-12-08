8c21cba8072657ac2d57eec2bb5bd1afc8f20ba7
Tim Hiatt <timhiatt@google.com>
2022-12-08 15:02:47 +0000

    reorder

--- ops/setup.sh
+++ ops/setup.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
-# GKE AutoNEG
+# Delete/Cleanup
 kubectl delete namespace autoneg-system
+kubectl delete namespace istio-system 
+kubectl delete namespace ops-argocd
+kubectl delete namespace app-time-now
+kubectl delete namespace app-bank-of-anthos
+
+# GKE AutoNEG
 kubectl apply -f autoneg/install-manifests.yaml || true
 
 # Istio Mesh
-kubectl delete namespace istio-system 
 kubectl apply -f istio/namespaces/namespace.yaml
 kubectl apply -f istio/install-manifests.yaml || true
 kubectl apply -f istio/install-manifests.yaml || true
 kubectl apply -f istio/plugins --recursive || true
 
 # Ops
-kubectl delete namespace ops-argocd
 kubectl apply -f argocd/namespaces/namespace.yaml
 kubectl apply -f argocd/install-manifests.yaml || true
 kubectl apply -f argocd/virtualService.yaml  -n ops-argocd || true
 
 # Apps Cluster
-kubectl delete namespace app-time-now
-kubectl delete namespace app-bank-of-anthos
-
 kubectl apply -f apps/namespaces --recursive || true
 kubectl apply -f apps/gateways --recursive || true
-
 
 # Apps Deploymentd
 kubectl apply -f ../apps/ --recursive || true
