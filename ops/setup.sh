6c03f0dc0724da223cef84a79f0963b5627c8006
Tim Hiatt <timhiatt@google.com>
2022-12-08 15:20:12 +0000

    reorder

--- ops/setup.sh
+++ ops/setup.sh
@@ -2,6 +2,7 @@
 
 # Delete/Cleanup
 kubectl delete namespace autoneg-system
+sleep 30
 kubectl delete namespace istio-system 
 kubectl delete namespace ops-argocd
 kubectl delete namespace app-time-now
