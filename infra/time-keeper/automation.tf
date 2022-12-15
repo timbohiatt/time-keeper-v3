90ff34e2b2f7d2ca887a38fc877a336093696107
Gary Bowers <gary@bowers1.com>
2022-12-15 14:35:18 +0000

    Add label for workload cluster

--- infra/time-keeper/automation.tf
+++ infra/time-keeper/automation.tf
@@ -1,11 +1,11 @@
 module "automation" {
-  source = "./modules/automation"
-  prefix = var.prefix    
-  billing_account = var.billing_account
-  folder_id = var.folder_id
-  region    = var.region
-  demo_name = "automation"
-  project_id = google_project.project.project_id
+  source            = "./modules/automation"
+  prefix            = var.prefix
+  billing_account   = var.billing_account
+  folder_id         = var.folder_id
+  region            = var.region
+  demo_name         = "automation"
+  project_id        = google_project.project.project_id
   network_self_link = module.vpc-hub.self_link
-  network_name = module.vpc-hub.name
+  network_name      = module.vpc-hub.name
 }
